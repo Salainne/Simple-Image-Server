07b926d436502295f73be7c38608f7b78af4db21
Salainne <email@salainne.com>
2025-04-26 15:56:17 +0200

    Initial

---
+++ Simple image server/Simple image server/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Simple_image_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnServertoggle = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnServertoggle
+            // 
+            this.btnServertoggle.Location = new System.Drawing.Point(155, 67);
+            this.btnServertoggle.Name = "btnServertoggle";
+            this.btnServertoggle.Size = new System.Drawing.Size(125, 52);
+            this.btnServertoggle.TabIndex = 0;
+            this.btnServertoggle.Text = "Start Server";
+            this.btnServertoggle.UseVisualStyleBackColor = true;
+            this.btnServertoggle.Click += new System.EventHandler(this.btnServertoggle_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(791, 595);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(765, 279);
+            this.txtLog.TabIndex = 1;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Simple image server";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 986);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1622, 32);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(179, 25);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1622, 1018);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnServertoggle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simple image server";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnServertoggle;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Simple image server/Simple image server/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_image_server
+{
+    public partial class Form1 : Form
+    {
+        enum LogLevel
+        {
+            Info,
+            Warning,
+            Error
+        }
+
+        private Thread serverThread;
+        private HttpListener httpListener;
+        private bool isRunning = false;
+        private LogLevel logLevel = LogLevel.Info;
+        private int _port = 9191;
+
+        private string _contentLocation = Application.StartupPath + "/content";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnServertoggle.Text = "Start Server";
+            toolStripStatusLabel1.Text = "Server is stopped";
+        }
+
+        private void btnServertoggle_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                serverThread = new Thread(StartServer);
+                serverThread.IsBackground = true;
+                isRunning = true;
+                serverThread.Start();
+                btnServertoggle.Text = "Stop Server";
+            }
+            else
+            {
+                isRunning = false;
+                httpListener.Stop();
+                btnServertoggle.Text = "Start Server";
+            }
+        }
+
+        private void StartServer()
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add($"http://localhost:{_port}/");
+
+            try
+            {
+                Invoke(new Action(() => Log($"Starting server on port: {_port}", LogLevel.Info)));
+                httpListener.Start();
+                toolStripStatusLabel1.Text = $"Server running on port {_port}";
+                while (isRunning)
+                {
+                    var context = httpListener.GetContext();
+                    var request = context.Request;
+                    var response = context.Response;
+                    if (request.HttpMethod == "GET")
+                    {
+                        string filePath = request.Url.LocalPath.TrimStart('/');
+                        string fullPath = System.IO.Path.Combine(_contentLocation, filePath);
+
+                        Invoke(new Action(() => Log($"GET: {filePath}", LogLevel.Info)));
+
+                        if (System.IO.File.Exists(fullPath))
+                        {
+                            byte[] imageBytes = System.IO.File.ReadAllBytes(fullPath);
+                            response.ContentType = "image/jpeg";
+                            response.ContentLength64 = imageBytes.Length;
+                            response.OutputStream.Write(imageBytes, 0, imageBytes.Length);
+                        }
+                        else
+                        {
+                            response.StatusCode = 404;
+                            byte[] errorMessage = Encoding.UTF8.GetBytes("File not found");
+                            response.OutputStream.Write(errorMessage, 0, errorMessage.Length);
+                        }
+                    }
+                    response.Close();
+                }
+            }
+            catch(HttpListenerException ex)
+            {
+                if (ex.NativeErrorCode != 995)
+                {
+                    MessageBox.Show("Error starting server: " + ex.Message);
+                }
+            }
+            Invoke(new Action(() => Log($"Server stopped", LogLevel.Info)));
+            toolStripStatusLabel1.Text = "Server is stopped";
+        }
+
+        private void Log(string message, LogLevel logLevel)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{DateTime.Now}: {message}");
+            sb.Append(string.Join(Environment.NewLine, txtLog.Lines.Take(10).ToArray()));
+
+            txtLog.Text = sb.ToString();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
